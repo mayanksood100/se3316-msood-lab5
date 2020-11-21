2503a3bffffbd00c75079677c0b04d112c42f319
Mayank Sood <mayanksood100@gmail.com>
2020-11-21 14:24:58 -0500

    Set up Router and Parsed Timetable Data as in Lab3

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+app.use("/", express.static("static"));
+app.use(express.json());
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+const fs = require("fs");
+const router = express.Router();
+router.use(express.json());
+const port = 3000;
+
+fs.readFile("./Lab3-timetable-data.json", "utf-8", (err, jsonString) => {
+    try {
+      data = JSON.parse(jsonString);
+    } catch (err) {
+      console.log("Error Parsing JSON ", err);
+    }
+  });
+
+app.use((req, res, next) => {
+console.log(`${req.method} request for ${req.url}`);
+next();
+  });
+  
+
+
+router.get('/courses', (req,res)=>{
+    res.send(data);
+});
+
+
+
+
+app.use("/api", router);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+  
